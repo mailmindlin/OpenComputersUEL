045dbaa10e53bc135f6c960957e24ebc301e1b66
mailmindlin <mailmindlin@users.noreply.github.com>
2026-01-24 15:03:21 -0500

    Start deprecating StackOption

--- src/main/kotlin/li/cil/oc/util/StackOption.kt
+++ src/main/kotlin/li/cil/oc/util/StackOption.kt
@@ -78,3 +78,7 @@ sealed class Either<out L, out R> {
 }
 
 fun ItemStack?.asStackOption(): StackOption = StackOption(this)
+fun ItemStack.asStackOption(): StackOption = SomeStack(this)
+
+fun ItemStack?.notEmpty(): ItemStack? = if (this == null || this.isEmpty) null else this
+fun ItemStack.notEmpty(): ItemStack? = if (this.isEmpty) null else this
