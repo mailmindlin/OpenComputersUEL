f162679689d0810968905a38c3fc58717afa28e7
mailmindlin <mailmindlin@users.noreply.github.com>
2026-01-24 22:23:21 -0500

    Fix JEI warning

--- src/main/kotlin/li/cil/oc/integration/jei/LootDiskCyclingRecipeHandler.kt
+++ src/main/kotlin/li/cil/oc/integration/jei/LootDiskCyclingRecipeHandler.kt
@@ -5,7 +5,9 @@ import li.cil.oc.api.Items
 import li.cil.oc.common.Loot
 import li.cil.oc.common.recipe.LootDiskCyclingRecipe
 import mezz.jei.api.ingredients.IIngredients
+import mezz.jei.api.ingredients.VanillaTypes
 import mezz.jei.api.recipe.BlankRecipeWrapper
+import mezz.jei.api.recipe.IIngredientType
 import mezz.jei.api.recipe.IRecipeWrapper
 import mezz.jei.api.recipe.IRecipeWrapperFactory
 import net.minecraft.item.ItemStack
@@ -15,20 +17,19 @@ object LootDiskCyclingRecipeHandler : IRecipeWrapperFactory<LootDiskCyclingRecip
         return LootDiskCyclingRecipeWrapper(recipe)
     }
 
-    class LootDiskCyclingRecipeWrapper(val recipe: LootDiskCyclingRecipe) : BlankRecipeWrapper() {
-
-        fun getInputs(): List<List<ItemStack>> {
+    class LootDiskCyclingRecipeWrapper(val recipe: LootDiskCyclingRecipe) : IRecipeWrapper {
+        private fun getInputs(): List<List<ItemStack>> {
             return listOf(
-                Loot.disksForCycling.toList(),
+                Loot.disksForCycling().toList(),
                 listOf(Items.get(Constants.ItemName.Wrench).createItemStack(1))
             )
         }
 
-        fun getOutputs(): List<ItemStack> = Loot.disksForCycling.toList()
+        private fun getOutputs(): List<ItemStack> = Loot.disksForCycling().toList()
 
         override fun getIngredients(ingredients: IIngredients) {
-            ingredients.setInputLists(ItemStack::class.java, getInputs())
-            ingredients.setOutputs(ItemStack::class.java, getOutputs())
+            ingredients.setInputLists(VanillaTypes.ITEM, getInputs())
+            ingredients.setOutputs(VanillaTypes.ITEM, getOutputs())
         }
     }
 }
