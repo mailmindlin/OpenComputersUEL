3107a841f392339c883de9af71d9ab5c96ea0eac
mailmindlin <mailmindlin@users.noreply.github.com>
2026-02-03 16:03:31 -0500

    correct bound

--- src/main/kotlin/li/cil/oc/util/OCObfuscationReflectionHelper.kt
+++ src/main/kotlin/li/cil/oc/util/OCObfuscationReflectionHelper.kt
@@ -3,7 +3,7 @@ package li.cil.oc.util
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper
 
 internal object OCObfuscationReflectionHelper {
-    fun <T, E> getPrivateValue(classToAccess: Class<in E?>?, instance: E?, srgName: String): T {
+    fun <T, E> getPrivateValue(classToAccess: Class<E>?, instance: E, srgName: String): T {
         // HACK: Don't break compatibility with older Forge versions.
         // This also works around a Scala compiler crash: "trying to do lub/glb of typevar ?E".
         return ObfuscationReflectionHelper.getPrivateValue(classToAccess, instance, *arrayOf(srgName))
